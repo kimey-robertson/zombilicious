2ca354cded54f0f1eed0efd5bfac0af098a65003
kimey-robertson <kimey@dealerstudio.com.au>
2025-06-28 13:46:11 +1000

    Inner grid on tiles

--- client/src/components/GameBoard/Tile.tsx
+++ client/src/components/GameBoard/Tile.tsx
@@ -11,6 +11,15 @@ const Tile = ({ tile }: TileProps) => {
   return (
     <div className="game-board-tile">
       <img src={tileImages[tile]} alt="tile" className="tile-image" />
+      <div className="tile-grid-overlay">
+        {Array.from({ length: 9 }, (_, index) => (
+          <div key={index} className="grid-cell">
+            <div className="grid-cell-inner">
+              {index}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
