fd056ba461e3f0e5651f4f04538e55c1a61692f6
kimey-robertson <kimey@dealerstudio.com.au>
2025-06-28 12:00:06 +1000

    Overlay dragging

--- client/src/components/Overlay/Overlay.tsx
+++ client/src/components/Overlay/Overlay.tsx
@@ -1,9 +1,11 @@
+import { usePlayerStore } from "../../store/usePlayerStore";
 import ResetBoard from "./ResetBoard";
 
 const Overlay = () => {
+  const isDragging = usePlayerStore((state) => state.isDragging);
   return (
     <div className="overlay-wrapper">
-      <div className="overlay">
+      <div className={`overlay ${isDragging ? "overlay-dragging" : ""}`}>
         <ResetBoard />
       </div>
     </div>
