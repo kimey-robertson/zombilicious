c179833968ad591a83a591cde63328e2c85c05ab
kimey-robertson <kimey@dealerstudio.com.au>
2025-06-28 12:07:24 +1000

    Improve drag and zoom

--- client/src/components/GameBoard.tsx
+++ client/src/components/GameBoard.tsx
@@ -13,7 +13,7 @@ const GameBoard = () => {
   const setOffset = usePlayerStore((state) => state.setOffset);
   const isDragging = usePlayerStore((state) => state.isDragging);
   const setIsDragging = usePlayerStore((state) => state.setIsDragging);
-  
+
   const lastPosition = useRef({ x: 0, y: 0 });
 
   const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
@@ -29,41 +29,6 @@ const GameBoard = () => {
     const dy = e.clientY - lastPosition.current.y;
     lastPosition.current = { x: e.clientX, y: e.clientY };
     setOffset((prev: Offset) => ({ x: prev.x + dx, y: prev.y + dy }));
-
-    // An attempt to make it so the grid can't be dragged out of the screen
-
-    // setOffset((prev) => {
-    //   const gridWidth = size * 300 * zoom;
-    //   const gridHeight = size * 300 * zoom;
-    //   const wrapperWidth = window.innerWidth;
-    //   const wrapperHeight = window.innerHeight;
-
-    //   let newX = prev.x + dx;
-    //   let newY = prev.y + dy;
-
-    //   if (gridWidth > wrapperWidth) {
-    //     const minX = wrapperWidth - gridWidth;
-    //     const maxX = 0;
-    //     newX = Math.min(maxX, Math.max(newX, minX));
-    //   }
-
-    //   if (gridHeight > wrapperHeight) {
-    //     const minY = wrapperHeight - gridHeight;
-    //     const maxY = 0;
-    //     newY = Math.min(maxY, Math.max(newY, minY));
-    //   }
-
-    //   return { x: newX, y: newY };
-    //   const maxX = 0;
-    //   const minX = wrapperWidth - gridWidth;
-    //   const maxY = 0;
-    //   const minY = wrapperHeight - gridHeight;
-
-    //   const newX = Math.min(maxX, Math.max(prev.x + dx, minX));
-    //   const newY = Math.min(maxY, Math.max(prev.y + dy, minY));
-
-    //   return { x: newX, y: newY };
-    // });
   };
 
   const handleMouseUp = () => {
@@ -72,17 +37,23 @@ const GameBoard = () => {
 
   const handleWheel = (e: React.WheelEvent<HTMLDivElement>) => {
     e.preventDefault();
-    const zoomAmount = e.deltaY > 0 ? -0.005 : 0.005;
+    const zoomAmount = e.deltaY > 0 ? -0.01 : 0.01;
     setZoom((prev: number) => Math.max(0.5, Math.min(prev + zoomAmount, 2)));
   };
 
   return (
-    <div className="game-board-wrapper">
+    <div
+      className="game-board-wrapper"
+      style={{
+        cursor: isDragging ? "grabbing" : "grab",
+      }}
+    >
       <div
         onMouseDown={handleMouseDown}
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
         onWheel={handleWheel}
+        onMouseLeave={handleMouseUp}
         className="game-board-grid"
         style={{
           transform: `translate(${offset.x}px, ${offset.y}px) scale(${zoom})`,
