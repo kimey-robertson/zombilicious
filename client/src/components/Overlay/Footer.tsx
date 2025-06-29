b5a71d5ff23005ad920a53b546e8704be40d0ae3
kimey-robertson <kimey@dealerstudio.com.au>
2025-06-29 22:48:01 +1000

    Actions remaining

--- client/src/components/Overlay/Footer.tsx
+++ client/src/components/Overlay/Footer.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { usePlayerStore } from "../../store/usePlayerStore";
 import "./Footer.css";
 import XPTracker from "./XPTracker";
+import ActionsRemaining from "./ActionsRemaining";
 
 const Footer = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,15 +24,18 @@ const Footer = () => {
       </div>
       {/* // Panel */}
       <div className={`footer-panel ${isOpen ? "open" : "closed"}`}>
-      <XPTracker />
-        <div className="footer-panel-content">
-          <h2>Action Panel</h2>
-          <div className="footer-panel-actions">
-            <button>Action 1</button>
-            <button>Action 2</button>
-            <button>Action 3</button>
-          </div>
+        <XPTracker />
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+          <ActionsRemaining />
         </div>
+        {/* <div className="footer-panel-content">
+            <h2>Action Panel</h2>
+            <div className="footer-panel-actions">
+                <button>Action 1</button>
+                <button>Action 2</button>
+                <button>Action 3</button>
+            </div>
+            </div> */}
       </div>
     </>
   );
