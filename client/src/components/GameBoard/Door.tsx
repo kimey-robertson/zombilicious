ee5232e4f232671a6d962444622d87d033e96c4e
kimey-robertson <kimey@dealerstudio.com.au>
2025-07-09 22:59:35 +1000

    Clickable doors

--- client/src/components/GameBoard/Door.tsx
+++ client/src/components/GameBoard/Door.tsx
@@ -1,18 +1,18 @@
 import { Door } from "../../../../shared/types";
 
 const DoorComponent = ({ door, cellId }: { door: Door; cellId: string }) => {
-  if (door.cellIds[0] !== cellId) return null;
+  if (door.cellIds[1] !== cellId) return null;
 
   return (
     <div
       className="door"
-      onClick={(e) => {
-        e.stopPropagation();
+      onClick={() => {
         console.log("door clicked");
       }}
       style={{
         transform: door.transform,
         backgroundColor: door.state === "open" ? "green" : "red",
+
       }}
     ></div>
   );
