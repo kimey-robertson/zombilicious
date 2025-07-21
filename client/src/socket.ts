59a73aa3903059c36c40e6f65289f79e45a33a63
kimey-robertson <kimey@dealerstudio.com.au>
2025-07-21 21:41:10 +1000

    Add production url

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -4,12 +4,17 @@ let socket: Socket | null = null;
 
 export const getSocket = (): Socket => {
   if (!socket) {
-    socket = io("http://localhost:8000", {
-      // Prevent frequent reconnections
-      reconnectionDelay: 1000,
-      reconnectionDelayMax: 5000,
-      reconnectionAttempts: 5,
-    });
+    socket = io(
+      process.env.NODE_ENV === "production"
+        ? "https://zombilicious-production.up.railway.app/"
+        : "http://localhost:8000",
+      {
+        // Prevent frequent reconnections
+        reconnectionDelay: 1000,
+        reconnectionDelayMax: 5000,
+        reconnectionAttempts: 5,
+      }
+    );
   }
   return socket;
 };
